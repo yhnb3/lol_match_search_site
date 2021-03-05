9aa67e5e8d7d726069adace74ebad0187aa865ea
yhnb3 <60080270+yhnb3@users.noreply.github.com>
2021-03-06 08:55:30 +0900

    2021-03-06-TIL

---
+++ 백엔드/backend/api/views.py
@@ -0,0 +1,43 @@
+from django.shortcuts import render
+from django.conf import settings
+
+# Create your views here.
+from django.http import HttpResponse, JsonResponse
+
+import requests, json
+
+RIOT_API_KEY = settings.RIOT_API_KEY
+
+def getAccount(name):
+    url = f'https://kr.api.riotgames.com/lol/summoner/v4/summoners/by-name/{name}'
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.190 Safari/537.36",
+        "Accept-Language": "ko-KR,ko;q=0.9,en-US;q=0.8,en;q=0.7",
+        "Accept-Charset": "application/x-www-form-urlencoded; charset=UTF-8",
+        "Origin": "https://developer.riotgames.com",
+        "X-Riot-Token": RIOT_API_KEY
+    }
+
+    response = requests.get(url, headers=headers)
+
+    return response.json()
+
+def getRecentMatches(request, summonerName):
+    summoner = getAccount(summonerName)
+    url = f'https://kr.api.riotgames.com/lol/match/v4/matchlists/by-account/{summoner["accountId"]}'
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.190 Safari/537.36",
+        "Accept-Language": "ko-KR,ko;q=0.9,en-US;q=0.8,en;q=0.7",
+        "Accept-Charset": "application/x-www-form-urlencoded; charset=UTF-8",
+        "Origin": "https://developer.riotgames.com",
+        "X-Riot-Token": RIOT_API_KEY
+    }
+    params = {
+        'beginIndex' : 0,
+        'endIndex' : 10
+    }
+    
+    response = JsonResponse(requests.get(url, headers=headers, params=params).json())
+    response['Access-Control-Allow-Origin'] = '*'
+    response['Content-Type'] = 'application/json;charset=utf-8'
+    return response 
